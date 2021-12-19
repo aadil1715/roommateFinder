c72378237982e09241c17de08975b54882ebb7be
Aadil Shaikh <aadilshaikh@kil-maclap-1.local>
2021-12-19 16:08:52 -0400

    Initial setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useReducer } from 'react';
 
+const formReducer = (state,event) =>{
+  return {
+    ...state,
+    [event.name] : event.value
+  }
+}
 function App() {
+  const [formData,setFormData] = useReducer(formReducer,{})
+  const [submitting,setSubmitting] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    setTimeout(() => {
+      setSubmitting(false)
+    }, 3000);
+    
+  }
+  const handleChange = event => {
+    setFormData({
+      name:event.target.name,
+      value:event.target.value,
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="form">
+      {submitting && 
+      <div> Submitting the form .. </div>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Username : </p>
+          </label>
+          <input type="text" name="name" onChange={handleChange}></input>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+   </div>
   );
 }
 
